e1c60a4da60fe3f7106a910186e6464743f2178f
hongShiX <litangdingzhen2024@163.com>
2025-01-06 15:52:36 +0800

    先搭个架子 日后再说

---
+++ README.md
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+# gin-admin-test
+
+> '测试'
+
+## Quick Start
+
+```bash
+make start
+```
+
+## Build
+
+```bash
+make build
+```
+
+## Generate wire inject files
+
+```bash
+make wire
+```
+
+## Generate swagger documents
+
+```bash
+make swagger
+```
+=======
+# my-gin-admin
+>>>>>>> aa598cf9124d18ebd960cd222f0b622a8b1266ea
+
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"github.com/hongShiX/cmd"
+	"github.com/urfave/cli/v2"
+)
+
+// Usage: go build -ldflags "-X main.VERSION=x.x.x"
+var VERSION = "v1.0.0"
+
+// @title ginadmintest
+// @version v1.0.0
+// @description '测试'
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @schemes http https
+// @basePath /
+func main() {
+	app := cli.NewApp()
+	app.Name = "ginadmintest"
+	app.Version = VERSION
+	app.Usage = "'测试'"
+	app.Commands = []*cli.Command{
+		cmd.StartCmd(),
+		cmd.StopCmd(),
+		cmd.VersionCmd(VERSION),
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
